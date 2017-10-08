f915f694e92421667f48a058c902504cf16021f7
Levisoto <levisoto.337@gmail.com>
2017-10-07 23:09:13 -0500

    first function from problem 1 completed

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,2 +1,20 @@
 module Lib where
 
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n    = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+fun1' :: [Integer] -> Integer
+fun1' n = foldl (*) 1 $ map (\x -> x - 2) $ filter even n
