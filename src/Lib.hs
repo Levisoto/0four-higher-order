bfe4951deee464923ce4862b8eda378390137eaf
Levisoto <levisoto.337@gmail.com>
2017-11-24 17:51:44 -0500

    Problem 2 completed. Is's my first time workining with Binary Tree

--- src/Lib.hs
+++ src/Lib.hs
@@ -22,3 +22,42 @@ fun1' n = foldl (*) 1 $ map (\x -> x - 2) $ filter even n
 fun2' :: Integer -> Integer
 fun2' 1 = 0
 fun2' n = sum $ filter even $ takeWhile (/=1) $ iterate (\ x -> if (even x) then (div x 2) else (3*x+1)) n
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+  -- Problem 2: Construct a balanced Tree
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
+
+foldTree :: Eq a => [a] -> Tree a
+foldTree xs = foldr insertt Leaf xs
+
+height :: Tree a -> Integer
+height Leaf =  0
+height (Node _ Leaf _ Leaf) = 0
+height (Node _ x _ y) = 1 + max (height x) (height y)
+
+insertt :: a -> Tree a -> Tree a 
+insertt a Leaf = Node 0 Leaf a Leaf
+insertt a (Node 0 Leaf x Leaf) = (Node 1 Leaf x (Node 0 Leaf a Leaf))
+insertt b ((Node 1 Leaf x (Node 0 Leaf a Leaf))) = (Node 1 (Node 0 Leaf b Leaf) x (Node 0 Leaf a Leaf))
+insertt a lis@(Node x left y right) 
+  | (((iscomplete left) && not (iscomplete right)) || iscomplete lis) = newright
+  | otherwise = newleft 
+    where
+      h x
+        | iscomplete lis = x + 1
+        | otherwise = x
+      newright = (Node (h x) left y (insertt a right))
+      newleft = (Node x (insertt a left) y right) 
+
+-- isbalanced :: Tree a -> Bool
+-- isbalanced Leaf = True
+-- isbalanced (Node _ left _ right) 
+--   | (abs $ (height left) - (height right)) <= 1 = True
+--   | otherwise = False
+
+iscomplete :: Tree a -> Bool
+iscomplete Leaf = False
+iscomplete (Node _ Leaf _ Leaf) = True
+iscomplete (Node _ left _ right) = (iscomplete left) && (iscomplete right) && (height left == height right)
